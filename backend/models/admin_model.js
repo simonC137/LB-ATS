70b204e2ceaf266ed4cc46f30271447d4d28657d
ahmed <ahmed5893hneynia@gmail.com>
2024-12-16 09:59:50 +0100

    first commit

---
+++ backend/models/admin_model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const adminSchema = new mongoose.Schema({
+ 
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+adminSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  next();
+});
+module.exports = mongoose.model('Admin', adminSchema);
---
+++ backend/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose'); 
+const Candidate = require('./models/candidate_model');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+// app.use(bodyParser.json());
+app.use(express.json());
+
+app.use('/auth',authRoutes)
+// Middleware
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.DB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err.message);
+        process.exit(1); 
+    });
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+
+
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
