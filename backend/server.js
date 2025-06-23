7229a586da476cb68bc79129b5963c8837293ef8
Simon Summers <simon7summers@gmail.com>
2025-06-23 22:42:46 +0100

    *13

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use('/candidates', cleanRejectedRoute);
 
 // Connect to MongoDB
 mongoose
-    .connect(process.env.DB_URI)
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => {
         console.error('Error connecting to MongoDB:', err.message);
