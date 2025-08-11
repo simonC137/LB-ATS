63b9920f9e537667cbf982c12000a2754930db0b
ahmed <ahmed5893hneynia@gmail.com>
2025-08-11 09:01:23 +0200

    fix import dotenv bug

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const dotenv = require('dotenv'); 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const express = require('express');
 const mongoose = require('mongoose'); 
